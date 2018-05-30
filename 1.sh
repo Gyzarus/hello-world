fa843a2791a37bb9c650773cf6db03c8f6374714
Gyzarus <35445284+Gyzarus@users.noreply.github.com>
2018-05-30 15:59:36 +0800

    Update and rename README.md to 1.sh

---
+++ 1.sh
@@ -0,0 +1,11 @@
+wget --no-check-certificate -O shadowsocks-libev.sh https://raw.githubusercontent.com/uxh/shadowsocks_bash/master/shadowsocks-libev.sh && bash shadowsocks-libev.sh << XXG
+xj2018
+443
+1
+
+XXG && wget --no-check-certificate -O appex.sh https://raw.githubusercontent.com/0oVicero0/serverSpeeder_Install/master/appex.sh && bash appex.sh install '2.6.32-642.el6.x86_64' << XXG
+
+n
+y
+y
+XXG
